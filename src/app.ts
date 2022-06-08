02b42733b379d83617abfb6ee6d473707c007fe6
huntmori <huntmori04x22@gmail.com>
2022-06-09 03:52:50 +0900

    app.ts  fix - add server class

--- src/app.ts
+++ src/app.ts
@@ -2,31 +2,59 @@ import * as express from 'express';
 import { Cat, CatType } from './cats/cats.model';
 import catsRouter from './cats/cats.route';
 
-const app: express.Express = express();
-const port: number = 3001;
-
-/** logging middleware */
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    console.log(req.rawHeaders[1]);
-    console.log('this is logging middleware');
-    next();
+class Server {
+  public app: express.Application;
+  public port: number = 3001;
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
   }
-);
 
-// *json middleware
-app.use(express.json());
-
-// router 사용 미들웨어
-app.use(catsRouter);
-
-/** 404 middleware */
-app.use(
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.send({ error: '404 not found' });
+  private setRouter() {
+    // router 사용 미들웨어
+    this.app.use(catsRouter);
   }
-);
 
-app.listen(port, () => {
-  console.log(`example app run on http://localhost:${port}`);
-});
+  private setMidleware() {
+    /** logging middleware */
+    this.app.use(
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) => {
+        console.log(req.rawHeaders[1]);
+        console.log('this is logging middleware');
+        next();
+      }
+    );
+
+    // *json middleware
+    this.app.use(express.json());
+    this.setRouter();
+    /** 404 middleware */
+    this.app.use(
+      (
+        req: express.Request,
+        res: express.Response,
+        next: express.NextFunction
+      ) => {
+        res.send({ error: '404 not found' });
+      }
+    );
+  }
+
+  public listen() {
+    this.setMidleware();
+    this.app.listen(this.port, () => {
+      console.log(`example app run on http://localhost:${this.port}`);
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
