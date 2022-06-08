6db4ae240ef31ed6b85d0491e0796a96113c3593
huntmori <huntmori04x22@gmail.com>
2022-06-08 23:58:05 +0900

    implement cat method -put -patch -delete

--- src/cats/cats.route.ts
+++ src/cats/cats.route.ts
@@ -61,4 +61,84 @@ router.post('/cats', (request: express.Request, response: express.Response) => {
   }
 });
 
+// *UPDATE : 고양이 데이터 업데이트 -> PUT
+router.put(
+  '/cats/:id',
+  (request: express.Request, response: express.Response) => {
+    try {
+      const params = request.params;
+      const body = request.body;
+      let result;
+
+      Cat.forEach(function (cat) {
+        if (cat.id === params.id) {
+          cat = body;
+          result = cat;
+        }
+      });
+      response.status(200).send({
+        success: true,
+        data: {
+          cat: result,
+        },
+      });
+    } catch (error: any) {
+      response.status(400).send({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+);
+// *UPDATE : 고양이 데이터 부분적으로 업데이트 -> PATCH
+router.patch(
+  '/cats/:id',
+  (request: express.Request, response: express.Response) => {
+    try {
+      const params = request.params;
+      const body = request.body;
+      let result;
+
+      Cat.forEach(function (cat) {
+        if (cat.id === params.id) {
+          cat = { ...cat, ...body };
+          result = cat;
+        }
+      });
+      response.status(200).send({
+        success: true,
+        data: {
+          cat: result,
+        },
+      });
+    } catch (error: any) {
+      response.status(400).send({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+);
+// *DELETE : 고양이 데이터 삭제 -> DELETE
+router.delete(
+  '/cats/:id',
+  (request: express.Request, response: express.Response) => {
+    try {
+      const params = request.params;
+      const body = request.body;
+
+      const newCat = Cat.filter((cat) => cat.id !== params.id);
+      response.status(200).send({
+        success: true,
+        data: newCat,
+      });
+    } catch (error: any) {
+      response.status(400).send({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+);
+
 export default router;
