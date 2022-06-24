5b25c631c4bf81469fe1242b8c91b807c129a444
huntmori <huntmori04x22@gmail.com>
2022-06-24 16:45:51 +0900

    update cats controller

--- nest/app/src/cats/cats.controller.ts
+++ nest/app/src/cats/cats.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Controller, Delete, Get, Patch, Put } from '@nestjs/common';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  getAllCat() {
+    return 'allCat';
+  }
+
+  @Get(':id')
+  getOneCat() {
+    return 'one cat';
+  }
+
+  @Put(':id')
+  updateCat() {
+    return 'update cat';
+  }
+
+  @Patch(':id')
+  updatePartialCat() {
+    return;
+  }
+
+  @Delete(':id')
+  deleteOne() {
+    return 'delete cat';
+  }
+}
