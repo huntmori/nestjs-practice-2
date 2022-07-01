3690aa27c3650a4aaf827d769477d4365ab005b9
huntmori <huntmori04x22@gmail.com>
2022-07-01 15:32:22 +0900

    Mongoose 와 Config를 적용해 보았습니다.

--- nest/app/src/app.module.ts
+++ nest/app/src/app.module.ts
@@ -4,9 +4,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CatsModule,
+    UsersModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
